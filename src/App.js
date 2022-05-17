94ef15f1f81a690466c1448e1f4eb8fd4fccd8f1
Gavriil84 <gabwt.84@gmail.com>
2022-05-16 22:37:24 -0700

    Updates sign in folder name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
-import SignIn from './routes/sign-in/authentication.component';
+import SignIn from './routes/authentication/authentication.component';
 
 const App = () => {
   return (
