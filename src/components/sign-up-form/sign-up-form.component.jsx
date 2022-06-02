54b63ced55d6521f0081d7a29632202a1cd1579f
Gavriil84 <gabwt.84@gmail.com>
2022-06-02 15:01:03 -0700

    Remove context from components

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,5 @@
-import { useState, useContext } from 'react';
+import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
-import { UserContext } from '../../contexts/user.context'
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
 
@@ -17,10 +16,6 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
-    const { setCurrentUser } = useContext(UserContext)
-
-    console.log('hit')
-
     const resetFormFields = () => {
         setFormFields(defaultFormFields)
     }
@@ -35,7 +30,6 @@ const SignUpForm = () => {
 
         try {
             const { user } = await createAuthUserWithEmailAndPassword(email, password);
-            setCurrentUser(user);
 
             await createUserDocumentFromAuth(user, { displayName });
             resetFormFields();
