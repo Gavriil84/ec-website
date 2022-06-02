cd7f919177a9606b77083787abc1c30a9d3f1499
Gavriil84 <gabwt.84@gmail.com>
2022-06-02 15:00:34 -0700

    Remove context from sign in component

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,9 +1,7 @@
-import { useState, useContext } from 'react';
+import { useState } from 'react';
 import { signInWithGooglePopup, createUserDocumentFromAuth, signinAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
-
-import { UserContext } from '../../contexts/user.context'
 
 import './sign-in-form.styles.scss';
 
@@ -15,8 +13,6 @@ const defaultFormFields = {
 const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
-
-    const { setCurrentUser } = useContext(UserContext);
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields)
@@ -32,7 +28,6 @@ const SignInForm = () => {
 
         try {
             const { user } = await signinAuthUserWithEmailAndPassword(email, password);
-            setCurrentUser(user);
             resetFormFields();
 
 
