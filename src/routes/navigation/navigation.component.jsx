2efb172c9bd02896c114c0be9d2b68d0d44a4a12
Gavriil84 <gabwt.84@gmail.com>
2022-06-02 15:02:44 -0700

    Optimize code

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,12 +7,7 @@ import './navigation.styles.scss';
 
 
 const Navigation = () => {
-    const { currentUser, setCurrentUser } = useContext(UserContext);
-
-    const signOutHandler = async () => {
-        await signOutUser();
-        setCurrentUser(null);
-    }
+    const { currentUser } = useContext(UserContext);
 
     return (
         <Fragment>
